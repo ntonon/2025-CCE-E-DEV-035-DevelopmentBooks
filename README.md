f15d7a1b306ecc20eaf717c3bffea5f0f0747932
nicolas.tonon <nicolas.tonon@eurekanetwork.org>
2026-01-26 00:03:29 +0100

    Update README ; Add Swagger documentation on controller

--- README.md
+++ README.md
@@ -2,25 +2,26 @@
 
 ### Description
 
-This application provides an RESTful API for user compute the price of their basket.
+This application provides a RESTful API for user to compute the price of their basket.
 
 It follows the description provided in https://stephane-genicot.github.io/DevelopmentBooks.html
 
 ### Technologies
 
 * Java 21
-* Maven ? version
-* Spring Boot ? version
-* Swagger UI ? version
+* Maven 3.*
+* Spring Boot 3.3.5
+* Swagger UI with OpenAPI 2.6.0
+* Junit Jupiter 5.10
 
 ### How to run project
 
-* Install Java
-* Install Maven
-* ...
+* Install Java 21 (or use IDE with Java embedded) → https://www.oracle.com/europe/java/technologies/downloads/#java21
+* Install Maven (or use IDE with Maven embedded) → https://maven.apache.org/install.html
+* Clone repository from https://github.com/ntonon/2025-CCE-E-DEV-035-DevelopmentBooks
+* To compile application, run "mvn clean -U install"
+* To launch application (on port 8080 by default), run "mvn spring-boot:run"
 
-### How to use the API
+### Swagger
 
-* Paths
-* No Auth
-* ...
+You can access the swagger from http://localhost:8080/swagger-ui/index.html. This page provides the contracts for APIs made available externally. It also gives user a quick tool to send HTTP request to the API and see the response.
--- src/main/java/com/example/developmentbooks/controller/StoreController.java
+++ src/main/java/com/example/developmentbooks/controller/StoreController.java
@@ -1,7 +1,15 @@
 package com.example.developmentbooks.controller;
 
+import com.example.developmentbooks.exception.NotFoundException;
+import com.example.developmentbooks.exception.NotNullException;
 import com.example.developmentbooks.model.dto.BasketDTO;
 import com.example.developmentbooks.service.BasketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +30,35 @@ public class StoreController {
         Pre: Receive valid basket
         Post: Return price of books in basket
      */
+    @Operation(summary = "Get price for basket")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Basket processed, price computed",
+                    content = @Content(
+                            schema = @Schema(implementation = Double.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "sample-price",
+                                            value = "{ 55.0 }"
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Basket provided is invalid",
+                    content = @Content(
+                            schema = @Schema(anyOf = {NotFoundException.class, NotNullException.class}),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "sample-error",
+                                            value = "{ Error message }"
+                                    )
+                            }
+                    )
+            )
+    })
     @GetMapping("/basket/price")
     public ResponseEntity<Double> getBasketPrice(@RequestBody BasketDTO basketDTO) {
         Double price = basketService.getPrice(basketDTO);
